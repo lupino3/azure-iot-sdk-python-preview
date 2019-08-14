9dc18dd811cb5b412c2ae35f8af995cd4a71e33e
olivakar <oliva.kar@microsoft.com>
2019-08-13 21:07:46 -0700

    2

---
+++ scripts/check_exists.py
@@ -0,0 +1,18 @@
+def is_intermediate_key_there():
+    if os.path.exists("demoCA/private/intermediate_key.pem"):
+        print("intermediate key present")
+    else:
+        print("intermediate key absent")
+
+
+def is_intermediate_cert_there():
+    if os.path.exists("demoCA/newcerts/intermediate_cert.pem"):
+        print("intermediate cert present")
+    else:
+        print("intermediate cert absent")
+
+
+if __name__ == '__main__':
+    is_intermediate_key_there()
+    is_intermediate_cert_there()
+
